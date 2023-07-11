4ba35920cef37eccc8f31e8717fc45c206475b1f
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-07-11 17:15:41 -0300

    import Scanner

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,12 +1,14 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
 
   /**
-   * Metodo main. opa
+   * Metodo main.
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
