6d011043e9c521a715ed4c78804961a2fc98eccd
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-07-11 17:14:56 -0300

    feat: create logic to calculate student result

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -25,7 +25,20 @@ public class App {
 
       System.out.print("Digite a nota obtida para " + activityName + ": ");
       int activityScore = Integer.parseInt(scanner.nextLine());
+
+      totalScore += activityScore * activityWeight;
+      totalWeight += activityWeight;
     }
+    double finalScore = (double) totalScore / totalWeight;
+
+    if (finalScore >= 85) {
+      System.out.print("Parabéns! Você alcançou " + finalScore + "%! E temos o prazer "
+          + "de informar que você obteve aprovação!");
+    } else {
+      System.out.print("Lamentamos informar que, "
+          + "com base na sua pontuação alcançada neste período, "
+          + finalScore + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
+
   }
 }
